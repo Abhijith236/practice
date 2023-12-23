077b545349a389bf5c6f605caea4d30a6473a0fb
Abhijith236 <aabhijith236@gmail.com>
2023-12-23 11:51:04 +0530

    ninth

--- src/Components/Workshop1.jsx
+++ src/Components/Workshop1.jsx
@@ -10,11 +10,7 @@ const Workshop1=()=>{
         borderBottom: '1.013px solid var(--Black-700, #282828)',
         
       };
-      const btnplus={
-        button:<svg width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
-                 <path fill-rule="evenodd" clip-rule="evenodd" d="M21.9042 9.32677C21.9042 8.92707 21.5802 8.60306 21.1805 8.60306C20.7808 8.60306 20.4568 8.92707 20.4568 9.32677V20.4237H9.36C8.96031 20.4237 8.63629 20.7477 8.63629 21.1474C8.63629 21.5471 8.96031 21.8711 9.36 21.8711H20.4568V32.968C20.4568 33.3677 20.7808 33.6917 21.1805 33.6917C21.5802 33.6917 21.9042 33.3677 21.9042 32.968V21.8711H33.0012C33.4009 21.8711 33.7249 21.5471 33.7249 21.1474C33.7249 20.7477 33.4009 20.4237 33.0012 20.4237H21.9042V9.32677Z" fill="#9D9FA2"/>
-                </svg>
-      }
+      
 
       const [isActive, setIsActive] = useState(false);
 
@@ -30,6 +26,7 @@ const Workshop1=()=>{
                     </svg>
                     <div className='text-sm'>Workshop</div>
                 </div>
+                
                 <div className='workshop1 d-flex  m-5 align-items-center justify-content-between'>
                     <img src={img1} alt="" />
                     <p className=''>Brainwave Robotics Workshop</p>
@@ -47,10 +44,11 @@ const Workshop1=()=>{
                         }
                     </button>
                 </div>
-                <div className='content1 d-flex flex-column  m-5 align-items-center  justify-content-lg-start' id='coll1' style={Border}>
+                <div className='content1 pb-3 d-flex flex-column  align-items-lg-start justify-content-center m-lg-5' id='coll1' style={Border}>
                     <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt minima magni corrupti accusantium, quo natus deleniti laudantium. Debitis modi pariatur consequuntur dolorum deserunt nobis? Distinctio in fugiat libero obcaecati nemo!</p>
                     <button className='btn btn-light' type='button'>Know More</button>
                 </div>
+                
                 <div className='workshop2 d-flex m-5 align-items-center justify-content-between '>
                     <img src={img2} alt="" />
                     <p className=''>Digital Signal Processing using Matlab</p>
@@ -72,6 +70,7 @@ const Workshop1=()=>{
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt minima magni corrupti accusantium, quo natus deleniti laudantium. Debitis modi pariatur consequuntur dolorum deserunt nobis? Distinctio in fugiat libero obcaecati nemo!</p>
                     <button className='btn btn-light' type='button'>Know More</button>
                 </div>
+                
                 <div className='workshop3 d-flex m-5 align-items-center  justify-content-sm-between'>
                     <img src={img3} alt="" />
                     <p>Quantum Signal Processing Workshop</p>
@@ -93,6 +92,7 @@ const Workshop1=()=>{
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt minima magni corrupti accusantium, quo natus deleniti laudantium. Debitis modi pariatur consequuntur dolorum deserunt nobis? Distinctio in fugiat libero obcaecati nemo!</p>
                     <button className='btn' type='button'>Know More</button>
                 </div>
+                
                 <div className='workshop4 d-flex m-5 align-items-center justify-content-between'>
                     <img src={img4} alt="" />
                     <p>Speech Recognition Workshop</p>
@@ -114,6 +114,7 @@ const Workshop1=()=>{
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt minima magni corrupti accusantium, quo natus deleniti laudantium. Debitis modi pariatur consequuntur dolorum deserunt nobis? Distinctio in fugiat libero obcaecati nemo!</p>
                     <button className='btn btn-light' type='button'>Know More</button>
                 </div>
+                
                 <div className='workshop5 d-flex m-5 align-items-center justify-content-between'>
                     <img src={img5} alt="" />
                     <p>The Blockchain and Signal Processing Workshop</p>
@@ -131,7 +132,7 @@ const Workshop1=()=>{
                         }
                     </button>
                 </div>
-                <div className='content5 pb-3' id='coll5' style={Border}>
+                <div className='content5 pb-3 ' id='coll5' style={Border}>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt minima magni corrupti accusantium, quo natus deleniti laudantium. Debitis modi pariatur consequuntur dolorum deserunt nobis? Distinctio in fugiat libero obcaecati nemo!</p>
                     <button className='btn btn-light' type='button'>Know More</button>
                 </div>
